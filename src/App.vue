43fb20a722c4554257cea84ad5530b828315771f
aayush1408 <aayushjaiswal0114@gmail.com>
2018-01-21 19:41:22 +0530

    displayed posts

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <posts-blog v-bind:posts='posts' ></posts-blog>
+  </div>
+</template>
+
+<script>
+import postsBlog from './components/postsBlog.vue';  
+export default {
+   components:{
+    'posts-blog':postsBlog
+  } ,
+
+   data(){
+    return{
+      posts:[{
+        title:'Rick',
+        message:'Rickty rickty mother fucker'
+      },{
+        title:'Morty',
+        message:'I am a loser'
+      },{
+        title:'Beth',
+        message:'I am Ricks daughters'
+      },{
+        title:'Jerry',
+        message:'I am just an asshole'
+      }]
+         }
+  }
+  
+}
+</script>
+
+<style>
+body{
+  margin: 0px;
+  font-family: 'Nunito SemiBold'
+}
+</style>
---
+++ src/components/postsBlog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul v-for="post in posts">
+      <li>{{post.message}} -- {{post.message}} </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props:['posts']
+}
+</script>
+
+<style>
+body{
+  margin: 0px;
+  font-family: 'Nunito SemiBold'
+}
+</style>
